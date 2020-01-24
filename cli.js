3eca18bf49b1fe4bee4a7da22601b69ab8542333
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-24 08:35:08 +0000

    Added comments to clean up code. Need to seperate concerns

--- cli.js
+++ cli.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 let componentName, componentType;
 
+// get arguments from command line
+
 const commander = require("commander")
   .arguments("<component-type>, <component-name>")
   .action((type, name) => {
@@ -14,7 +16,15 @@ const commander = require("commander")
   })
   .parse(process.argv);
 
+// Main function of script, needs to be split into smaller functions
+
 const createComponent = (type, name) => {
+  // Ensure component name starts with capital letter
+
+  name = name[0].toUpperCase() + name.slice(1);
+
+  // If arguments not given, prevent code from running
+
   if (
     typeof componentName === "undefined" ||
     typeof componentType === "undefined"
@@ -25,9 +35,13 @@ const createComponent = (type, name) => {
     process.exit(1);
   }
 
+  // File path setup
+
   const src = path.resolve("src");
   const typeOfComponent = path.resolve("src/" + type);
   const nameOfComponent = path.resolve("src/" + type + "/" + name);
+
+  // Check if folders exist and create them if they don't
 
   if (!fs.existsSync(src)) {
     fs.mkdirSync(src);
@@ -43,6 +57,8 @@ const createComponent = (type, name) => {
     fs.mkdirSync(nameOfComponent);
     console.log(`${nameOfComponent} folder created!`);
   }
+
+  // Boilerplate code for the files
 
   const jsx = `import React from "react";
 import styles from "./${name}.module.scss";
@@ -74,10 +90,14 @@ describe("${name} tests", () => {
 export default ${name};
 `;
 
+  // Create the files we use for React
+
+  fs.writeFileSync(`${nameOfComponent}/index.js`, index);
   fs.writeFileSync(`${nameOfComponent}/${name}.jsx`, jsx);
   fs.writeFileSync(`${nameOfComponent}/${name}.module.scss`, "");
   fs.writeFileSync(`${nameOfComponent}/${name}.test.js`, test);
-  fs.writeFileSync(`${nameOfComponent}/index.js`, index);
 };
+
+// Run function when command given
 
 createComponent(componentType, componentName);
