f76d910bc7b01bbce9f92717560f53b32ff3dca4
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-24 12:53:00 +0000

    rewrote how file content is generated

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ const commander = require("commander")
   })
   .parse(process.argv);
 
-// Main function of script, needs to be split into smaller functions
-
-const createComponent = (type, name) => {
-  // Ensure component name starts with capital letter
-
-  name = name[0].toUpperCase() + name.slice(1);
-
-  // If arguments not given, prevent code from running
-
+const checkArguments = () => {
   if (
     typeof componentName === "undefined" ||
     typeof componentType === "undefined"
@@ -33,6 +25,23 @@ const createComponent = (type, name) => {
     );
     process.exit(1);
   }
+};
+
+const checkFolderExists = folder => {
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+    console.log(`${folder} folder created!`);
+  }
+};
+
+const createComponent = (type, name) => {
+  // If arguments not given, prevent code from running
+
+  checkArguments();
+
+  // Ensure component name starts with capital letter
+
+  name = name[0].toUpperCase() + name.slice(1);
 
   // File path setup
 
@@ -42,59 +51,64 @@ const createComponent = (type, name) => {
 
   // Check if folders exist and create them if they don't
 
-  if (!fs.existsSync(src)) {
-    fs.mkdirSync(src);
-    console.log(`${src} folder created!`);
-  }
-
-  if (!fs.existsSync(typeOfComponent)) {
-    fs.mkdirSync(typeOfComponent);
-    console.log(`${typeOfComponent} folder created!`);
-  }
-
-  if (!fs.existsSync(nameOfComponent)) {
-    fs.mkdirSync(nameOfComponent);
-    console.log(`${nameOfComponent} folder created!`);
-  }
+  checkFolderExists(src);
+  checkFolderExists(typeOfComponent);
+  checkFolderExists(nameOfComponent);
 
   // Boilerplate code for the files
 
-  const jsx = `import React from "react";
-import styles from "./${name}.module.scss";
+  const jsx = [
+    `import React from "react";`,
+    `import styles from "./${name}.module.scss";`,
+    "",
+    `const ${name} = () => {`,
+    `  return (`,
+    `    <>`,
+    `      <p>${name} works</p>`,
+    `    </>`,
+    `  );`,
+    `};`,
+    "",
+    `export default ${name};`
+  ];
 
-const ${name} = () => {
-  return ( 
-    <>
-      <p>${name} works</p>
-    </>
-  );
-};
+  const test = [
+    `import React from "react";`,
+    `import { ${name} } from "./${name}";`,
+    "",
+    `describe("${name} tests", () => {`,
+    `  let component;`,
+    "",
+    `  beforeEach(() => {`,
+    `    component =`,
+    `  })`,
+    `});`
+  ];
 
-export default ${name};
-`;
-
-  test = `import React from "react";
-import { ${name} } from "./${name}";
-
-describe("${name} tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component = 
-  })
-});`;
-
-  const index = `import ${name} from "./${name}";
-
-export default ${name};
-`;
+  const index = [
+    `import ${name} from "./${name}";`,
+    "",
+    `export default ${name};`
+  ];
 
   // Create the files we use for React
 
-  fs.writeFileSync(`${nameOfComponent}/index.js`, index);
-  fs.writeFileSync(`${nameOfComponent}/${name}.jsx`, jsx);
+  jsx.forEach(line => {
+    fs.appendFileSync(`${nameOfComponent}/${name}.jsx`, line);
+    fs.appendFileSync(`${nameOfComponent}/${name}.jsx`, "\n");
+  });
+
+  test.forEach(line => {
+    fs.appendFileSync(`${nameOfComponent}/${name}.test.js`, line);
+    fs.appendFileSync(`${nameOfComponent}/${name}.test.js`, "\n");
+  });
+
+  index.forEach(line => {
+    fs.appendFileSync(`${nameOfComponent}/index.js`, line);
+    fs.appendFileSync(`${nameOfComponent}/index.js`, "\n");
+  });
+
   fs.writeFileSync(`${nameOfComponent}/${name}.module.scss`, "");
-  fs.writeFileSync(`${nameOfComponent}/${name}.test.js`, test);
 
   console.log(
     `Component with name: ${name} has been created in ${type} folder`
